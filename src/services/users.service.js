d6932e03985c3f4a8b73bed924217e484085d371
Mariela Belen Rascon marielabrascon@gmail.com <marielabrascon@gmail.com>
2022-11-17 16:32:29 -0300

    hash passwords, create user

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,14 +1,15 @@
 import models from '../models/index.js'
 import empty from 'is-empty'
+import bcrypt from 'bcrypt'
 
+const saltRounds = 10
 
-// categories management
+// users management
 class UsersService {
   constructor() {
     // this.generate()
   }
 
-  // WIP Manager user, creator of Uncategorized category
   async generate() {
     const limit = 1
     
@@ -16,23 +17,51 @@ class UsersService {
       const user = new models.UserModel({
         username: 'muwella',
         email: 'marielabrascon@gmail.com',
-        password: 'someVerySecurePassword'
+        password: await bcrypt.hash('someVerySecurePassword', saltRounds)
       })
       
       await user.save()
     }
   }
 
-  async create() {
+  async hash_password(password) {
+    return await bcrypt.hash(password, saltRounds)
+  }
+
+  async create(user) {
+    // validate schema
+
+    // hash password
+    user.password = await this.hash_password(user.password)
+
+    // try to save on DB
+    try {
+      userinDB = new models.UserModel({user})
+      await userinDB.save()
+    } catch(err) {
+      console.error(err)
+    }
   }
 
   async get_users() {
+    return await models.UserModel.find()
   }
 
   async get_user_by_id(id) {
+    const user = await models.UserModel.findById(id)
+
+    if ( empty(user) ) {
+      throw new Error('User not found')
+    } else {
+      return user
+    }
   }
   
   async update() {
+  }
+
+  async partial_update() {
+
   }
 
   async delete() {
