229f961e0abded90f5fc2a7b03c4a965580bfa64
Mariela Belen Rascon marielabrascon@gmail.com <marielabrascon@gmail.com>
2022-11-17 16:32:02 -0300

    GET users

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,21 @@
 import express from 'express'
+import UsersService from '../services/users.service.js'
 
 const router = express.Router()
+const service = new UsersService()
+
+
+// endpoints
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await service.get_users()
+    res.status(200).json(users)
+  } catch(err) {
+    console.error(err)
+  }
+})
+
 
 router.get('/:id', (req, res) => {
     const { id } = req.params
