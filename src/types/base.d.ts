5970b5c4d7c06fa47e190a673fcc1b1d41678bff
Mariela Belen Rascon <marielabrascon@gmail.com>
2023-02-06 22:55:59 -0300

    Changing camelCase to snake_case

--- src/types/base.d.ts
+++ src/types/base.d.ts
@@ -3,6 +3,6 @@ import { Types } from "mongoose"
 // basic data that gets repeated all across database
 export interface BaseModel {
 	readonly id: Types.ObjectId,
-	readonly createdAt: Date,
-	updatedAt: Date
+	readonly created_at: Date,
+	updated_at: Date
 }
