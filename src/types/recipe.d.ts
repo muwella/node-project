38d6ae73119dcb72dbea5c4b3adf77f23196963a
Mariela Belen Rascon <marielabrascon@gmail.com>
2023-02-06 22:55:32 -0300

    Recipe models

--- src/types/recipe.d.ts
+++ src/types/recipe.d.ts
@@ -1,31 +1,17 @@
 import { BaseModel } from "./base.d.ts"
 import { Types } from "mongoose"
 
-interface RecipeBase extends BaseModel {
+// received from user
+export interface RecipeInCreate {
     name: string,
-    instructions: string,
-    ingredients: Types.ObjectId[],
-    creator_id: Types.ObjectId,
+    readonly creator_id: Types.ObjectId,
+    instructions?: string,
+    ingredients?: string[],
+    categories?: Types.ObjectId[],
 }
 
 // sent to user
-// this scheme has DB added info
-interface UserInResponse extends UserBase {
-    is_confirmed: boolean,
-    is_active: boolean,
-}
+export interface RecipeInDB extends BaseModel, RecipeInCreate {}
 
 // received from user
-interface UserInCreate extends UserBase {
-    password: string
-}
-
-// received from user
-interface UserInUpdate extends Partial<UserBase>{}
-
-// -----
-
-interface UserLogin {
-    username: string,
-    password: string
-}
+export type RecipeInUpdate = Partial<RecipeInCreate>
